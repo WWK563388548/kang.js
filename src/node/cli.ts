dc26b9200d36bdeb073e982186fc8fe6c5f1a6d1
Weikai Wang <wc563388548@gmail.com>
2023-05-11 23:27:46 +0900

    feat: building basic of dev server

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,4 +1,6 @@
 import { cac } from "cac";
+import path = require("path");
+import { createDevServer } from "./dev";
 
 const version = require("../../package.json").version;
 const cli = cac("kang").version(version).help();
@@ -6,7 +8,10 @@ const cli = cac("kang").version(version).help();
 cli.command("[root]", "start dev server")
   .alias("dev")
   .action(async (root: string) => {
-    console.log("dev", root);
+    root = root ? path.resolve(root) : process.cwd();
+    const server = await createDevServer(root);
+    await server.listen();
+    server.printUrls();
   });
 
 cli.command("build [root]", "build for production")
