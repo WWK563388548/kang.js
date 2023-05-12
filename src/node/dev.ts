93ee7d2164f142366d0b1345f9fc157463a137af
Weikai Wang <wc563388548@gmail.com>
2023-05-12 20:56:48 +0900

    feat: adding HMR

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer as createViteDevServer } from "vite";
 import { indexHtmlPlugin } from "./plugin_kang/indexHtml";
+import pluginReact from "@vitejs/plugin-react";
 
 export async function createDevServer(root = process.cwd() ) {
   return createViteDevServer({
     root,
-    plugins: [indexHtmlPlugin()],
+    plugins: [indexHtmlPlugin(), pluginReact()],
   });
 }
--- src/node/plugin_kang/indexHtml.ts
+++ src/node/plugin_kang/indexHtml.ts
@@ -12,6 +12,7 @@ export function indexHtmlPlugin(): Plugin {
           let html = await readFile(DEFAULT_HTML_PATH, "utf-8");
 
           try {
+            html = await server.transformIndexHtml(req.url, html, req.originalUrl);
             res.statusCode = 200;
             res.setHeader("Content-Type", "text/html");
             res.end(html);
