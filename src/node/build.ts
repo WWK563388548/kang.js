b6e6fc8c4a11dbddff04ee01799412a11fc47937
Weikai Wang <wc563388548@gmail.com>
2023-05-12 21:34:08 +0900

    feat: adding bundle logic of build

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,3 +1,44 @@
-export async function build(root: string = process.cwd()) {
-  
+import { build as viteBuild } from "vite";
+import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from "./constants";
+
+export async function bundle(root: string) {
+  const clientBuild = async () => {
+    return viteBuild({
+      mode: "production",
+      root,
+      build: {
+        outDir: "build",
+        rollupOptions: {
+          input: CLIENT_ENTRY_PATH,
+          output: {
+            format: "esm",
+          }
+        },
+      }
+    });
+  }
+
+  const serverBuild = async () => {
+    return viteBuild({
+      mode: "production",
+      root,
+      build: {
+        ssr: true,
+        outDir: ".temp",
+        rollupOptions: {
+          input: SERVER_ENTRY_PATH,
+          output: {
+            format: "cjs",
+          }
+        },
+      }
+    });
+  }
+
+  await clientBuild();
+  await serverBuild();
+}
+
+export async function build(root: string) {
+  await bundle(root);
 }
