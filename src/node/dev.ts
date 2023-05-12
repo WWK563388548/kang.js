075898c0ea5ea1aaf1179527cf7b8fca973ff8dc
Weikai Wang <wc563388548@gmail.com>
2023-05-12 22:18:10 +0900

    fix: removing unnecessary code

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,7 +2,7 @@ import { createServer as createViteDevServer } from "vite";
 import { indexHtmlPlugin } from "./plugin_kang/indexHtml";
 import pluginReact from "@vitejs/plugin-react";
 
-export async function createDevServer(root = process.cwd() ) {
+export async function createDevServer(root ) {
   return createViteDevServer({
     root,
     plugins: [indexHtmlPlugin(), pluginReact()],
