e9ee787d389d659f3b7391dc91446f7f909342d9
Weikai Wang <wc563388548@gmail.com>
2023-05-12 21:25:32 +0900

    fix: refactoring HMR logic

--- src/node/plugin_kang/indexHtml.ts
+++ src/node/plugin_kang/indexHtml.ts
@@ -1,11 +1,27 @@
 import { readFile } from "fs/promises";
 import { Plugin } from "vite";
-import { DEFAULT_HTML_PATH  } from "../constants";
+import { DEFAULT_HTML_PATH, CLIENT_ENTRY_PATH  } from "../constants";
 
 export function indexHtmlPlugin(): Plugin {
   return {
     name: "kang:index-html",
     apply: "serve",
+    // insert script tag to index.html
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+            },
+            injectTo: "body",
+          }
+        ],
+      }
+    },
     configureServer(server) { 
       return () => {
         server.middlewares.use(async (req, res, next) => {
