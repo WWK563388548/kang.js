324f58608949e5d41609f369dca833c8b9ec5bcf
Weikai Wang <wc563388548@gmail.com>
2023-05-12 19:31:31 +0900

    feat: let vite handle template html

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,9 @@
 import { createServer as createViteDevServer } from "vite";
+import { indexHtmlPlugin } from "./plugin_kang/indexHtml";
 
 export async function createDevServer(root = process.cwd() ) {
   return createViteDevServer({
     root,
+    plugins: [indexHtmlPlugin()],
   });
 }
---
+++ src/node/plugin_kang/indexHtml.ts
@@ -0,0 +1,25 @@
+import { readFile } from "fs/promises";
+import { Plugin } from "vite";
+import { DEFAULT_HTML_PATH  } from "../constants";
+
+export function indexHtmlPlugin(): Plugin {
+  return {
+    name: "kang:index-html",
+    apply: "serve",
+    configureServer(server) { 
+      return () => {
+        server.middlewares.use(async (req, res, next) => {
+          let html = await readFile(DEFAULT_HTML_PATH, "utf-8");
+
+          try {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end(html);
+          } catch (error) {
+            return next(error);
+          }
+        });
+      };
+    },
+  };
+}
