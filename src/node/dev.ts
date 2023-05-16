3d9f6c7f347a7c5a28f38bbc5837f6768413cc41
Weikai Wang <wc563388548@gmail.com>
2023-05-16 23:37:34 +0900

    feat: resolving configuration file

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,8 +1,11 @@
 import { createServer as createViteDevServer } from 'vite';
 import { indexHtmlPlugin } from './plugin_kang/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
+import { resolveConfig } from './config';
 
 export async function createDevServer(root) {
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log(config);
   return createViteDevServer({
     root,
     plugins: [indexHtmlPlugin(), pluginReact()]
---
+++ src/shared/types/index.ts
@@ -0,0 +1,37 @@
+import { UserConfig as ViteConfiguration } from 'vite';
+
+export type NavitemWithLink = {
+  text: string;
+  link: string;
+};
+
+export interface Sidebar {
+  [path: string]: SidebarGroup[];
+}
+
+export interface SidebarGroup {
+  text?: string;
+  items: SidebarItem[];
+}
+
+export type SidebarItem =
+  | { text: string; link: string }
+  | { text: string; link?: undefined; items: SidebarItem[] };
+
+export interface Footer {
+  message?: string;
+  copyright?: string;
+}
+
+export interface ThemeConfig {
+  nav?: NavitemWithLink[];
+  sidebar?: Sidebar;
+  footer?: Footer;
+}
+
+export interface UserConfig {
+  title?: string;
+  description?: string;
+  themeConfig?: ThemeConfig;
+  vite?: ViteConfiguration;
+}
